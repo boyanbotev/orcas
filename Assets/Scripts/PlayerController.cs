749f9af4eb6c0c3c3afc54778057b7b00be539bb
Boyan Botev <boyanbotev@gmail.com>
2025-05-25 16:44:38 +0300

    feat: add movement system

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+enum OrcaState
+{
+    Swimming,
+    Boosting,
+    Recharging
+}
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float boostSpeed = 10f;
+    [SerializeField] private float boostDuration = 0.2f;
+    [SerializeField] private float boostCooldown = 1f;
+    [SerializeField] private float turnSpeed = 5f;
+    [SerializeField] private Vector2 movementInput;
+    [SerializeField] private OrcaState currentState = OrcaState.Swimming;
+    [SerializeField] private float minXRotation = -50f;
+    [SerializeField] private float maxXRotation = 50f;
+
+    Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Boost(InputAction.CallbackContext context)
+    {
+        if (context.phase != InputActionPhase.Performed || currentState != OrcaState.Swimming)
+            return;
+
+        currentState = OrcaState.Boosting;
+        StartCoroutine(BoostRoutine());
+    }
+
+    public void SetDir(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        var speed = currentState == OrcaState.Boosting ? boostSpeed : moveSpeed;
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(movementInput.y * turnSpeed * Time.deltaTime, movementInput.x * turnSpeed * Time.deltaTime, 0));
+        rb.AddForce(transform.forward * speed * Time.deltaTime, ForceMode.VelocityChange);
+        float xRotation = ClampAngle(rb.rotation.eulerAngles.x, minXRotation, maxXRotation);
+        rb.rotation = Quaternion.Euler(xRotation, rb.rotation.eulerAngles.y, 0);
+    }
+
+    float ClampAngle(float val, float min, float max)
+    {
+        float relRange = (max - min) / 2f;
+        float offset = max - relRange;
+        float z = ((val + 540) % 360) - 180 - offset;
+
+        if (Mathf.Abs(z) > relRange)
+        {
+            val = relRange * Mathf.Sign(z) + offset;
+        }
+
+        Debug.Log(val+"..."+ min+"..."+ max);
+        //if (val < min)
+            //val = min;
+        //else if (val > max)
+            //val = max;
+        return val;
+    }
+
+    IEnumerator BoostRoutine()
+    {
+        yield return new WaitForSeconds(boostDuration);
+        currentState = OrcaState.Recharging;
+        yield return new WaitForSeconds(boostCooldown);
+        currentState = OrcaState.Swimming;
+    }
+}
