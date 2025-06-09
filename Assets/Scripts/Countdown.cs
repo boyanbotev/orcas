107cf912f881193d37045e9307e419bee119970e
Boyan Botev <boyanbotev@gmail.com>
2025-06-09 18:07:06 +0300

    fix: red dolphins and GOAL text

--- Assets/Scripts/Countdown.cs
+++ Assets/Scripts/Countdown.cs
@@ -11,11 +11,28 @@ public class Countdown : MonoBehaviour
         GameManager.OnCountdownStart += Count;
         countdownText = GetComponent<TextMeshProUGUI>();
         countdownText.color = new Color(1, 1, 1, 0);
+
+        Ball.OnHitOpponentWall += DisplayPlayerGoal;
+        Ball.OnHitHomeWall += DisplayOpponentGoal;
     }
 
     private void OnDisable()
     {
         GameManager.OnCountdownStart -= Count;
+        Ball.OnHitOpponentWall -= DisplayPlayerGoal;
+        Ball.OnHitHomeWall -= DisplayOpponentGoal;
+    }
+
+    private void DisplayPlayerGoal()
+    {
+        countdownText.text = "GOAL";
+        countdownText.color = new Color(1, 0, 0, 1);
+    }
+
+    private void DisplayOpponentGoal()
+    {
+        countdownText.text = "ENEMY GOAL";
+        countdownText.color = new Color(1, 0, 0, 1);
     }
 
     private void Count()
